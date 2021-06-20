85a55659417419eadcbf217945c16697cd3e38bf
sungjin cho <nightfuryfly@gmail.com>
2021-06-20 14:08:18 +0900

    Initial commit

---
+++ dev/serve.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import Vue from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { VueScopedStoreSample } from '@/entry.esm';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  // components: {
+  //  VueScopedStoreSample,
+  // }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vue-scoped-store-sample />
+  </div>
+</template>
---
+++ vue-scoped-store.d.ts
@@ -0,0 +1,7 @@
+import Vue, { PluginFunction, VueConstructor } from 'vue';
+
+
+declare const VueScopedStore: PluginFunction<any>;
+export default VueScopedStore;
+
+export const VueScopedStoreSample: VueConstructor<Vue>;
