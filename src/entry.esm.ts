bae782d5d0c764852dc3738b9aaee2d359f2394b
sungjin cho <nightfuryfly@gmail.com>
2021-08-08 13:56:19 +0900

    fix

--- src/entry.esm.ts
+++ src/entry.esm.ts
@@ -1,7 +1,7 @@
 import _Vue, { PluginFunction } from 'vue';
 
 // Import vue components
-import * as components from '@/lib-components/index';
+// import * as components from '@/lib-components/index';
 import ScopedStoreMixin from './scoped-store/scoped-store-mixin';
 
 import {sendGlobalData, setGlobalDataCallback, sendGlobalCommand, setGlobalCommandCallback} from './scoped-store/methods/global-data-callback';
@@ -12,15 +12,6 @@ const install: PluginFunction<any> = function installVueScopedStore(Vue: typeof 
 
   Vue.mixin(ScopedStoreMixin);
 
-  /*
-      $setGlobalDataCallback(key:string, callback: (data: any) => void, path?:string) : void;
-      $sendPageData(data:any, path?:string) : void;
-      $setPageDataCallback(callback: (data: any) => void) : void;
-      $sendPageCommand(command:string, argument?:any) : void;
-      $setPageCommandCallback(callback: (command:string, argument?:any) => void) : void;
-      $sendGlobalCommand(key:string, command:string, argument?:any) : void;
-      $setGlobalCommandCallback(key:string, callback: (command: string, argument?: any) => void) : void;
-  */
   Vue.prototype.$sendGlobalData = sendGlobalData;
   Vue.prototype.$setGlobalDataCallback = setGlobalDataCallback;
   Vue.prototype.$sendGlobalCommand = sendGlobalCommand;
@@ -31,9 +22,9 @@ const install: PluginFunction<any> = function installVueScopedStore(Vue: typeof 
   Vue.prototype.$sendPageCommand = sendPageCommand;
   Vue.prototype.$setPageCommandCallback = setPageCommandCallback;
 
-  Object.entries(components).forEach(([componentName, component]) => {
-    Vue.component(componentName, component);
-  });
+  // Object.entries(components).forEach(([componentName, component]) => {
+  //   Vue.component(componentName, component);
+  // });
 };
 
 // Create module definition for Vue.use()
